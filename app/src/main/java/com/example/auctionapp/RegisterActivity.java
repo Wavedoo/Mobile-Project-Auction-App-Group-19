743e3ee3dae84dc04dfd6e8423421376f38cec67
Sterling <sterling1@hotmail.ca>
2023-11-05 20:48:43 -0500

    Created login and register activities

---
+++ app/src/main/java/com/example/auctionapp/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.auctionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}

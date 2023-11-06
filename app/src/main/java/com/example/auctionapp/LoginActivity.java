2e59f63c1ace8e5d990adfe1e9f1f9fa582b4307
Sterling <sterling1@hotmail.ca>
2023-11-05 21:26:45 -0500

    Added navigation

--- app/src/main/java/com/example/auctionapp/LoginActivity.java
+++ app/src/main/java/com/example/auctionapp/LoginActivity.java
@@ -1,15 +1,36 @@
 package com.example.auctionapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends FragmentActivity {
-
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.registerButton);
     }
+
+    public void handleSignUp(View v) {
+        // Handle the click somehow
+        startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+    }
+
+/*    public void onChanged(View v){
+        if(usernameEditText.getText().toString() != null && passwordEditText.getText().toString() != null){
+            loginButton.setEnabled(true);
+        }else{
+            loginButton.setEnabled(false);
+        }
+    }*/
 }
--- app/src/main/java/com/example/auctionapp/RegisterActivity.java
+++ app/src/main/java/com/example/auctionapp/RegisterActivity.java
@@ -1,15 +1,40 @@
 package com.example.auctionapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends FragmentActivity {
-
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private EditText confirmPasswordEditText;
+    private Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        confirmPasswordEditText = findViewById(R.id.editTextConfirmPassword);
+        registerButton = findViewById(R.id.registerButton);
     }
+    public void handleRegister(View v){
+        //TODO: Handle registering user to database
+        finish();
+    }
+
+    public void handleHasAccount(View v){
+        finish();
+    }
+
+/*    public void onChanged(View v){
+        if(usernameEditText.getText().toString() != null && passwordEditText.getText().toString() != null && confirmPasswordEditText.getText().toString() != null){
+            registerButton.setEnabled(true);
+        }else{
+            registerButton.setEnabled(false);
+        }
+    }*/
 }
